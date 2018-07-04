0aa1e64a675a2be69a305d79e6cd5e0739657867
codlab <codlabtech@gmail.com>
2018-07-04 17:31:24 +0200

    updating receiving call activities according to new version

--- app/src/main/java/fr/voxeet/sdk/sample/activities/IncomingCallActivity.java
+++ app/src/main/java/fr/voxeet/sdk/sample/activities/IncomingCallActivity.java
@@ -1,47 +1,20 @@
 package fr.voxeet.sdk.sample.activities;
 
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 
 import sdk.voxeet.com.toolkit.activities.notification.AbstractIncomingCallActivity;
-import sdk.voxeet.com.toolkit.activities.notification.IncomingCallFactory;
-import sdk.voxeet.com.toolkit.activities.workflow.VoxeetAppCompatActivity;
 
 /**
- * In this snippet, `IncomingCallActivity` extends `AbstractIncomingCallActivity`. A method must be overriden :
- * ```
- *
- * @NonNull
- * @Override protected Class<? extends VoxeetAppCompatActivity> getActivityClassToCall() {
- * return MainActivity.class;
- * }
- * ```
- * <p>
- * Note that in the case of multiple Activities in the app, you can use the VoxeetAppCompatActivity to automatically register the activity to start when "accepting" a call.
- * Instead of returning `MainActivity.class` in the sample, use `IncomingCallFactory.getAcceptedIncomingActivityKlass()`:
- * <p>
- * ```
- * @NonNull
- * @Override protected Class<? extends VoxeetAppCompatActivity> getActivityClassToCall() {
- * Class<? extends VoxeetAppCompatActivity> temp = IncomingCallFactory.getAcceptedIncomingActivityKlass();
- * <p>
- * if(null != temp)
- * return temp;
- * <p>
- * return MainActivity.class;
- * }
- * <p>
- * ```
+ * Minimalist Activity receiving incoming calls from the SDK
  */
 
 public class IncomingCallActivity extends AbstractIncomingCallActivity {
-    @NonNull
+
     @Override
-    protected Class<? extends VoxeetAppCompatActivity> getActivityClassToCall() {
-        Class<? extends VoxeetAppCompatActivity> temp = IncomingCallFactory.getAcceptedIncomingActivityKlass();
+    protected void onCreate(Bundle savedInstanceState) {
+        //specific Voxeet implement is done here
+        super.onCreate(savedInstanceState);
 
-        if (null != temp)
-            return temp;
-
-        return MainActivity.class;
+        //no need to implement setContentView when using AbstractIncomingCallActivity
     }
 }
